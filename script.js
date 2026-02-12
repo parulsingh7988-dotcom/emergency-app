734f17ff6ae611edf0c2abcf8e52aff13d572094
parulsingh7988-dotcom <parulsingh7988@gmail.com>
2026-02-12 16:32:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function login() {
+    alert("Login Successful!");
+    window.location.href = "dashboard.html";
+}
+function saveSecret() {
+    alert("Secret information saved successfully!");
+}
+function triggerEmergency() {
+    alert("Emergency alert sent to trusted contact!");
+}
